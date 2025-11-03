986a0ee98a6dd3d101e7fc23b7e43e5c4974c03c
asdqdsa <asdqdsa@protonmail.com>
2025-11-03 04:35:59 +0100

    feat: add sliding on mousedown

--- src/features/theme-switcher/index.js
+++ src/features/theme-switcher/index.js
@@ -1,12 +1,36 @@
 import { createElement } from '@/shared/dom/create-element';
 
+let isActive = false;
+
 export function ThemeButton({ emit }) {
+  const mouseDownHandler = () => {
+    console.log('Mouse down');
+    isActive = true;
+    emit('theme:next');
+  };
+  const mouseUpHandler = () => {
+    isActive = false;
+    console.log('Mouse up');
+  };
+  const mouseLeaveHandler = () => {
+    console.log('Mouse leave');
+  };
+  const mouseOverHandler = () => {
+    if (isActive) emit('theme:next');
+    console.log('Mouse over');
+  };
+
+  console.log('isActive: ', isActive);
   return createElement(
     'button',
     {
       className: 'btn',
-      onClick: () => emit('theme:next'),
+      onMouseDown: mouseDownHandler,
+      onMouseUp: mouseUpHandler,
+      onMouseLeave: mouseLeaveHandler,
+      onMouseOver: mouseOverHandler,
     },
+
     'Next Theme1'
   );
 }
